3fff7323e0fbb59f90d18c1724d328830385d037
eli-azure <71076078+eli-azure@users.noreply.github.com>
2020-09-13 17:00:31 +0300

    Update install-nginx.sh
    Get the second ip machine

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,11 +1,38 @@
 #!/bin/bash -xv
+
+myIp=$(ip address show dev eth0 | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
+myIpPre=$(echo $myIp | cut -d'.' -f1-3)
+myIpPost=$(echo $myIp | cut -d'.' -f4)
+ipUp=$((myIpPost + 1))
+ipUp=$myIpPre.$ipUp
+ipDown=$((myIpPost - 1))
+ipDown=$myIpPre.$ipDown
+
+limit=5
+i=0
+while true
+do
+	ping -c1 $ipUp
+	if [[ $? == 0 ]]; then
+		st2=$ipUp
+		break
+	fi
+	ping -c1 $ipDown
+        if [[ $? == 0 ]]; then
+                st2=$ipDown
+                break
+        fi
+	((i++))
+	[[ $i == $limit ]]&& break; then
+done
+
 apt update
 apt install -y nginx
 cat > /etc/nginx/nginx.conf <<EOF
 events { }
 http {
 	upstream beckend {
-		server 10.0.0.4:80;
+		server $st2:80;
 		}
 	server {
 		listen 443 ssl;
