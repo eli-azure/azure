e12164a5138ecde65bd89661c76128bf74e0a511
eli-azure <71076078+eli-azure@users.noreply.github.com>
2020-09-13 19:42:13 +0300

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -49,3 +49,11 @@ http {
 EOF
 #systemctl restart nginx
 
+apt install -y openssl
+
+cd /etc/nginx
+mkdir ssl
+chmod 700 ssl
+yes "" | openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/vm.key -out  /etc/nginx/ssl/vm.crt
+#nginx -t 
+systemctl reload nginx
